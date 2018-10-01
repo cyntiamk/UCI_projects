83e8f1f6257504f7673ad0cee40335396433dd17
cyntiamk <cyntiamk@gmail.com>
2018-10-01 15:46:04 -0700

    updating mySQL HW upto 7h

--- mySQL_HW/mySQL_HW.sql
+++ mySQL_HW/mySQL_HW.sql
@@ -60,10 +60,17 @@ effort. Delete the description column.*/
 ALTER TABLE actor DROP COLUMN Description;
 
 #4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(last_name) AS number_actors
+FROM actor
+GROUP BY last_name
+ORDER BY last_name;
 
 /*
 4b. List last names of actors and the number of actors who have that last name, 
 but only for names that are shared by at least two actors*/
+SELECT last_name, COUNT(last_name) AS number_actors
+FROM actor
+WHERE number_actors >= 2;
 
 /*4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as
  GROUCHO WILLIAMS. Write a query to fix the record.*/
@@ -165,22 +172,104 @@ WHERE title = "Alone Trip";
 /*7c. You want to run an email marketing campaign in Canada, 
 for which you will need the names and email addresses of all Canadian customers.
  Use joins to retrieve this information.*/
- 
+SELECT  first_name, last_name, email, sub1.*
+FROM ( 
+			  SELECT sub.*,country
+			  FROM ( SELECT address.address_id, city.country_id
+			                 FROM address
+                             INNER JOIN city
+                             ON address.city_id = city.city_id
+							) sub
+               INNER JOIN country
+               ON sub.country_id = country.country_id
+               ) sub1
+INNER JOIN customer
+ON sub1.address_id = customer.address_id
+WHERE country = "Canada";
+
  
 /*7d. Sales have been lagging among young families, and you wish to 
 target all family movies for a promotion. Identify all movies categorized as family
  films.*/
- 
+SELECT sub.*, `name`
+ FROM ( 
+			  SELECT film.title, film_category.category_id
+			  FROM film
+			  INNER JOIN film_category
+			  ON film.film_id = film_category.film_id
+              ) sub
+INNER JOIN category
+ON sub.category_id = category.category_id
+WHERE `name` = "Family";
  
 #7e. Display the most frequently rented movies in descending order.
+SELECT sub.*,rental_id 
+FROM (
+			 SELECT  inventory.inventory_id, film.title
+             FROM film
+             INNER JOIN inventory
+             ON film.film_id = inventory.film_id
+              )sub
+INNER JOIN rental
+ON sub.inventory_id = rental.inventory_id
+GROUP BY title, rental_id;
 
 #7f. Write a query to display how much business, in dollars, each store brought in.
-
+SELECT  store_id,  SUM(payment.amount) AS store_total
+FROM payment
+INNER JOIN staff
+ON payment.staff_id = staff.staff_id
+GROUP BY store_id;
 
 #7g. Write a query to display for each store its store ID, city, and country.
+SELECT store_id, sub.*
+FROM(
+            SELECT sub1.*,address_id
+	        FROM(
+                        SELECT  city.city, country.country, city.city_id
+		                FROM country
+                        INNER JOIN city
+                        ON country.country_id = city.country_id
+                        )sub1
+			 INNER JOIN address
+             ON sub1.city_id = address.city_id
+			 ) sub
+INNER JOIN store
+ON sub.address_id = store.address_id;
+
+
 /*7h. List the top five genres in gross revenue in descending order.
  (Hint: you may need to use the following tables: 
  category, film_category, inventory, payment, and rental.)*/
+SELECT sub.* ,SUM(amount)AS gross_revenue
+ FROM(
+			SELECT sub1.*, rental.rental_id
+            FROM(
+                            SELECT sub2.*, inventory.inventory_id
+                            FROM(
+										SELECT category.`name`, film_category.film_id
+										 FROM category
+                                         INNER JOIN film_category
+                                         ON category.category_id = film_category.category_id
+                                         ) sub2
+							INNER JOIN inventory
+                            ON sub2.film_id = inventory.film_id
+                            ) sub1
+			INNER JOIN rental
+            ON sub1.inventory_id = rental.inventory_id
+            )sub
+INNER JOIN payment
+ON sub.rental_id = payment.rental_id
+GROUP BY `name`
+ORDER BY  gross_revenue DESC;
+
+
+            
+                                         
+              
+							
+											
+                                            
  
 /*8a. In your new role as an executive, 
 you would like to have an easy way of viewing the Top five genres by gross revenue.
