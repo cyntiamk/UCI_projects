f613da0cb2e1d16eb84e61d409e1c13ee8d139c8
Cyntia Wonsch <cyntiamk@gmail.com>
2018-10-24 11:18:13 -0700

    Delete config.py

--- web_scrapping_HW/config.py
+++
@@ -1,7 +0,0 @@
-consumer_key = "JMGs200cdVJvTXCQ39lLC1Sn6"
-
-consumer_secret = "hMueOGVOgp9cOejzhNwDcy0Dd8GBh9DYp9EjCU3zus1aNzHsSS"
-
-access_token = "1031760895880228864-7dI5fDHeKCH9JSyWiltl2mYnYsMC9i"
-
-access_token_secret = "AP2jXaKuHVS7Q8gLiKOvTEsK7cNasrAarYKSunmadNRx4"
