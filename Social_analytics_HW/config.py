b6afddaed2321e3277df45a5dcb9ed68c39adef5
Cyntia Wonsch <cyntiamk@gmail.com>
2018-10-23 22:14:44 -0700

    Delete config.py

--- Social_analytics_HW/config.py
+++
@@ -1,9 +0,0 @@
-consumer_key = "ByzglTt6py5NGkU1O2jdpOmhj"
-
-consumer_secret = "xi1LNWLBfYnwuyPJiZHxUCOMl0US6cNyjCWUNJOXV0HH85FaF5"
-
-access_token = "1031760895880228864-VdQZtiOzBHP9lVp995LHf3g1jK1i4F"
-
-access_token_secret = "AHZNyvr4Y1IVJT6TA3EYnR5twR2eUmAgGQOkaxo8zjNSB"
-
-
