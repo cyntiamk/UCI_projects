22e2f64f611d12b7829db016473422f4b2499e37
cyntiamk <cyntiamk@gmail.com>
2018-10-28 18:04:25 -0700

    commented scripts out

--- web_scrapping/app.py
+++ web_scrapping/app.py
@@ -6,15 +6,22 @@ app = Flask(__name__)
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+# create route to connect the app
 @app.route('/')
 def index():
+	# create variable to hold all items in the database
 	mars_data = mongo.db.mars_data.find_one()
+	# render index.html template and pass data retrieved from database
 	return render_template('index.html', mars_data=mars_data)
 
+# create route to connect the app to the function
 @app.route('/scrape')
 def scrape():
+	# call the same variable that holds all the items in the database
 	mars_data = mongo.db.mars_data
+	# call the function that holds the dict with all the scrapped items
 	data = scrape_mars.scrape()
+	# add each item int a dictionary
 	mars_data.update(
 		{},
 		data,
--- web_scrapping/scrape_mars.py
+++ web_scrapping/scrape_mars.py
@@ -1,3 +1,4 @@
+# Import dependencies and libraries
 import pandas as pd
 import os
 from bs4 import BeautifulSoup as bs
@@ -9,14 +10,16 @@ import tweepy
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
 
-
+# create function to automate the chomedriver browser 
 def init_browser():
     executable_path = {'executable_path':'/usr/local/bin/chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
 
+# create function to hold all the information scrapped
 def scrape():
     browser = init_browser()
 
+    # emprty dictionary to hold the scrapped information
     mars_data = {}
 
     # NASA Mars Latest News
@@ -24,23 +27,27 @@ def scrape():
     browser.visit(news_url)
     news_html = browser.html
 
+    # use beautiful soup to parse the html and extract title and news paragraph
     news_soup = bs(news_html, 'html.parser')
     items = news_soup.find_all('ul',class_='item_list')
     for item in items:
         mars_data['news_title'] = item.find('div',class_='content_title').text
         mars_data['news_p'] = item.find('div', class_='article_teaser_body').text
-    #browser.quit()    
+  
 
     # JPL Mars Space Images
     img_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(img_url)
     img_html = browser.html
     img_soup = bs(img_html, 'html.parser')
-
+    # use beautiful soup to parse the html and extract the link on the first page to 
+    # get to the page with the link with the large size image
     feat = img_soup.find('footer')
     data_link = feat.find('a')['data-link']
+    # with the data link scrapped, create the url with the link to the full image
     data_img = ('https://www.jpl.nasa.gov/' + data_link)
 
+    # call the url scrapped above and scrape the url with the full image
     browser.visit(data_img)
     feat_img_html = browser.html
     feat_img_soup = bs(feat_img_html, 'html.parser')
@@ -48,9 +55,12 @@ def scrape():
     feat_img_href = feat_img.find('a')['href']
 
     mars_data['featured_image_url'] = (f'https://www.jpl.nasa.gov/' + feat_img_href)
-    #browser.quit()
-
+    
     #Mars Weather
+    # for the current weather, I decided it would be more ethical to 
+    # use the twitter API since it was discussed in class that Twitter provides 
+    # free public API to try to prevent people from scrapping their page
+
     # Setup Tweepy API authentications
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -68,8 +78,12 @@ def scrape():
     # Mars Facts
     facts_url = "https://space-facts.com/mars/"
 
+    # used pandas 
     table = pd.read_html(facts_url)
 
+    # extracted the table, since it was only on table on this url
+    # set the index to 0 dropped the header and index
+    # store the table in a string in the dictionary
     mars_table_to_html =table[0].to_html(header=False, index=False)
     mars_data['fact'] = mars_table_to_html
 
@@ -81,7 +95,8 @@ def scrape():
     link_list = []
     url_list = []
 
-
+    # first rendered the front page of hemisphers and scrapped the pages with full
+    # image of each hemisphere
     hem_html = browser.html
     hem_soup = bs(hem_html, 'html.parser')
     results = hem_soup.find_all('div',class_='item')
@@ -91,7 +106,10 @@ def scrape():
         
     url_list = ['https://astrogeology.usgs.gov/'+ link for link in link_list]
 
+    # empty list to store a dictonaries with each hemispheres img url and title
     hemisphere_images = []
+
+    # looped through each page and rendered the img url and title
 
     for page in url_list:      
         page_url = page
@@ -106,6 +124,7 @@ def scrape():
         hem_dict = {'title':title,
                     'img_url':img_url}
         hemisphere_images.append(hem_dict)
+    # store list of dictionaries into the main dictionary
     mars_data['hemisphere_images']=hemisphere_images
 
     browser.quit()    
